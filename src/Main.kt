db5014fc786075443beb541ea034ba5868248eb6
Sergei <sk_developer@mail.ru>
2023-09-29 19:32:40 +0300

    Задание 8

---
+++ src/Main.kt
@@ -0,0 +1,33 @@
+// Миксин с базовым поведением
+interface Printable {
+    fun print()
+}
+
+// Реализация базового поведения
+class BasicPrintable : Printable {
+    override fun print() {
+        println("Printing...")
+    }
+}
+
+// Миксин с дополнительным поведением
+interface Colorable {
+    fun setColor(color: String)
+}
+
+// Реализация дополнительного поведения
+class BasicColorable : Colorable {
+    override fun setColor(color: String) {
+        println("Setting color to $color")
+    }
+}
+
+// Класс, который использует миксины
+class Printer : Printable by BasicPrintable(), Colorable by BasicColorable()
+
+fun main() {
+    val printer = Printer()
+
+    printer.print() // Вывод: Printing...
+    printer.setColor("Red") // Вывод: Setting color to Red
+}
